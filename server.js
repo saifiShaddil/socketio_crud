d7bc605d91c36464f0f8cdc60df028ba2b2b3796
saifiShaddil <shaddilsaifi021296@gmail.com>
2022-07-16 12:26:29 +0530

    access control origin changed

--- server.js
+++ server.js
@@ -39,6 +39,12 @@ const server = http.createServer(app);
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use('/api/users', usersRouter);
 
